6cdaab6057c27ef37278fb38c4d249ca2fe69ec6
intrnl <intrnl28+git@gmail.com>
2020-07-30 21:42:21 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function $ (query, root = document) {
+  return root.querySelector(query);
+}
+
+/** @type {HTMLLIElement} */
+let todoItemTemplate = $('template.todo-item').content.firstElementChild;
+
+/** @type {HTMLUListElement} */
+let todoList = $('.root .todo-list');
+/** @type {HTMLFormElement} */
+let todoHeaderForm = $('.root .todo-header');
+/** @type {HTMLInputElement} */
+let todoHeaderInput = $('.root .todo-header-input');
+
+
+todoHeaderForm.addEventListener('submit', (ev) => {
+  ev.preventDefault();
+
+  let value = todoHeaderInput.value;
+  todoHeaderInput.value = '';
+
+  let item = todoItemTemplate.cloneNode(true);
+  $('.todo-item-text', item).textContent = value;
+  $('.todo-item-actions', item).addEventListener('click', (ev) => {
+    item.parentNode.removeChild(item);
+  });
+
+  todoList.append(item);
+});
